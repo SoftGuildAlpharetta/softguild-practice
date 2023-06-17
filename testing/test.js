442851625d7ceacc1ccf2ae8496a252716983a54
Jonathan Ling <jonathanling@shamrocksweb.com>
2023-06-17 13:08:21 -0400

    Initial commit after project creation.

---
+++ testing/test.js
@@ -0,0 +1,36 @@
+// const someJson = {
+//     someProperty: "hello, Dolphin may get retro achievement support in the future"
+// };
+
+// class SomeClass {
+//     constructor() {
+//         this.someProperty = "hello, PCSX2 has retro achievement support now.";
+//     }
+// }
+
+// const someObject = new SomeClass();
+
+// console.log(someObject.someProperty);
+// console.log(someJson.someProperty);
+
+// const arrowParseJsonResponse = (response) => response.json();
+
+// function parseJsonResponse(response) {
+//     return response.json();
+// }
+
+// const pokemonGames = fetch('https://pokeapi.co/api/v2/generation/generation-i')
+// .then(parseJsonResponse)
+// .then(responseJson => void 0);
+
+function getVendingItem(amount, item) {
+  switch (item) {
+    case "Soda":
+      if (amount >= 2.5) {
+        return { item: "Coca-Cola", change: amount - 2.5 };
+      }
+      return "Not enough money for Coca-Cola";
+  }
+}
+
+console.log(getVendingItem.toString);
