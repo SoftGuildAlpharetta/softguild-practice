5ebdcec8fe2d00b4a7a28c797bfd1ee4246e2da1
Jonathan Ling <jonathan.ling@hoteleffectiveness.com>
2023-06-26 21:03:32 -0400

    Refactor to a class.

--- lib/VendingMachineService.ts
+++ lib/VendingMachineService.ts
@@ -9,53 +9,50 @@ interface VendingItemResponse {
   error?: string;
 }
 
-export function getVendingItem(
-  amount: number,
-  item: string,
-  quantity: number
-): VendingItemResponse {
-  const vendingItems: Record<string, VendingItem> = {
-    Soda: {
-      quantity: 45,
-      amount: 2.5,
-    },
-    Gum: {
-      quantity: 100,
-      amount: 1.0,
-    },
-    Doritos: {
-      quantity: 2,
-      amount: 3.75,
-    },
-    'Wasabi Peas': {
-      quantity: 10,
-      amount: 5.75,
-    },
-    Cheetos: {
-      quantity: 10,
-      amount: 2.5,
-    },
-  };
+interface VendingItemRequest {
+  amount: number;
+  item: string;
+  quantity: number;
+}
 
-  // Does this item exist
-
-  const itemToVend = vendingItems[item];
-  if (itemToVend === undefined) {
-    return { item, error: 'No item found' };
+export class VendingMachineService {
+  private _inventory: Record<string, VendingItem>;
+  constructor(inventory: Record<string, VendingItem>) {
+    this._inventory = inventory;
   }
 
-  // "Do we have it in stock"
-
-  if (itemToVend.quantity < quantity) {
-    return { item, error: 'Not enough in stock' };
+  public updateInventory(itemName: string, inventoryItem: VendingItem) {
+    this._inventory[itemName] = inventoryItem;
   }
 
-  // "Does the nerd have enough dosh"
-
-  const grandTotalOfRequestedItems = itemToVend.amount * quantity;
-
-  if (amount >= grandTotalOfRequestedItems) {
-    return { item, change: amount - grandTotalOfRequestedItems };
+  public setInventory(inventory: Record<string, VendingItem>) {
+    this._inventory = inventory;
   }
-  return { item, error: `Not enough money for ${item}` };
+
+  public getVendingItemResponse(
+    request: VendingItemRequest
+  ): VendingItemResponse {
+    const { item, amount, quantity } = request;
+    // Does this item exist
+
+    const itemToVend = this._inventory[item];
+    if (itemToVend === undefined) {
+      return { item, error: 'No item found' };
+    }
+
+    // "Do we have it in stock"
+
+    if (itemToVend.quantity < quantity) {
+      return { item, error: `Not enough ${item} in stock` };
+    }
+
+    // "Does the nerd have enough dosh"
+
+    const grandTotalOfRequestedItems = itemToVend.amount * quantity;
+
+    if (amount >= grandTotalOfRequestedItems) {
+      return { item, change: amount - grandTotalOfRequestedItems };
+    }
+    return { item, error: `Not enough money for ${item}` };
+  }
 }
--- tests/VendingMachineService.spec.ts
+++ tests/VendingMachineService.spec.ts
@@ -1,45 +1,117 @@
-import { getVendingItem } from '../lib/VendingMachineService';
+import { VendingMachineService } from '../lib/VendingMachineService';
 
 describe('VendingMachineService', () => {
   describe('#getVendingItem', () => {
     test('invalid item should return an error property', () => {
-      const result = getVendingItem(999.99, 'Invalid Item', 0);
+      const service = new VendingMachineService({
+        Item1: { amount: 4, quantity: 2 },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 999.99,
+        item: 'Invalid Item',
+        quantity: 1,
+      });
       expect(result.error).toBeTruthy();
     });
 
     test('doritos should return change correctly', () => {
-      const result = getVendingItem(8.5, 'Doritos', 2);
-      expect(result.change).toBe(1.0);
+      const service = new VendingMachineService({
+        Doritos: { amount: 4.5, quantity: 5 },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 4.5,
+        item: 'Doritos',
+        quantity: 1,
+      });
+      expect(result.change).toBe(0);
     });
 
     test('Wasabi Peas should return correctly', () => {
-      const result = getVendingItem(12.0, 'Wasabi Peas', 2);
-      console.log(result);
-      expect(result.change).toBe(0.5);
+      const service = new VendingMachineService({
+        'Wasabi Peas': {
+          quantity: 10,
+          amount: 5.75,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 6.0,
+        item: 'Wasabi Peas',
+        quantity: 1,
+      });
+      expect(result.change).toBe(0.25);
     });
 
     test('Gum should return correctly', () => {
-      const result = getVendingItem(1.5, 'Gum', 1);
+      const service = new VendingMachineService({
+        Gum: {
+          quantity: 100,
+          amount: 1.0,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 1.5,
+        item: 'Gum',
+        quantity: 1,
+      });
       expect(result.change).toBe(0.5);
     });
 
     test('Soda should return change correctly', () => {
-      const result = getVendingItem(3.25, 'Soda', 1);
+      const service = new VendingMachineService({
+        Soda: {
+          quantity: 45,
+          amount: 2.5,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 3.25,
+        item: 'Soda',
+        quantity: 1,
+      });
       expect(result.change).toBe(0.75);
     });
 
     test('Cheetos should return an error for quantity', () => {
-      const result = getVendingItem(5.5, 'Cheetos', 999999);
-      expect(result.error).toContain('Not enough in stock');
+      const service = new VendingMachineService({
+        Cheetos: {
+          quantity: 10,
+          amount: 2.5,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 5.5,
+        item: 'Cheetos',
+        quantity: 999999,
+      });
+      expect(result.error).toContain('Not enough Cheetos in stock');
     });
-
     test('Multiple items should return correct change.', () => {
-      const result = getVendingItem(15, 'Cheetos', 2);
+      const service = new VendingMachineService({
+        Cheetos: {
+          quantity: 10,
+          amount: 2.5,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 15,
+        item: 'Cheetos',
+        quantity: 2,
+      });
       expect(result.change).toBe(10);
     });
 
     test('Not enough for multiple items should return an error.', () => {
-      const result = getVendingItem(4.5, 'Cheetos', 2);
+      const service = new VendingMachineService({
+        Cheetos: {
+          quantity: 10,
+          amount: 2.5,
+        },
+      });
+      const result = service.getVendingItemResponse({
+        amount: 4.5,
+        item: 'Cheetos',
+        quantity: 2,
+      });
       expect(result.error).toContain('Not enough money for');
     });
   });
